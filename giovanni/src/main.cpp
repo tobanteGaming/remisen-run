0421d9900d312fefcc3c2a419b19991d156c9078
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-03-18 18:00:16 +0100

    Cleanup

--- giovanni/src/main.cpp
+++ giovanni/src/main.cpp
@@ -2,42 +2,15 @@
 #include "SFML/Graphics.hpp"
 
 int main() {
-  // Create the main window
   sf::RenderWindow window(sf::VideoMode(800, 600), "Giovanni");
-  //   // Load a sprite to display
-  //   sf::Texture texture;
-  //   if (!texture.loadFromFile("cute_image.jpg"))
-  //     return EXIT_FAILURE;
-  //   sf::Sprite sprite(texture);
-  //   // Create a graphical text to display
-  //   sf::Font font;
-  //   if (!font.loadFromFile("arial.ttf"))
-  //     return EXIT_FAILURE;
-  //   sf::Text text("Hello SFML", font, 50);
 
-  //   // Load a music to play
-  //   sf::Music music;
-  //   if (!music.openFromFile("nice_music.ogg"))
-  //     return EXIT_FAILURE;
-  //   // Play the music
-  //   music.play();
-
-  // Start the game loop
   while (window.isOpen()) {
-    // Process events
     sf::Event event;
     while (window.pollEvent(event)) {
-      // Close window: exit
       if (event.type == sf::Event::Closed)
         window.close();
     }
-    // Clear screen
     window.clear({25, 25, 25});
-    // Draw the sprite
-    // window.draw(sprite);
-    // // Draw the string
-    // window.draw(text);
-    // Update the window
     window.display();
   }
   return EXIT_SUCCESS;
