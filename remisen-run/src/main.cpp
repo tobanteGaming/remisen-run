60641cb04eb69bd8e3747037126f97f14ded9553
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-03-19 22:08:40 +0100

    Draw stuff

--- remisen-run/src/main.cpp
+++ remisen-run/src/main.cpp
@@ -4,15 +4,19 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Remisen Run");
-
+    sf::RectangleShape rectangle(sf::Vector2f(250.f, 50.f));
+    rectangle.setPosition(100, 100);
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed) window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
         }
+
         window.clear({25, 25, 25});
+        window.draw(rectangle);
         window.display();
     }
     return EXIT_SUCCESS;
